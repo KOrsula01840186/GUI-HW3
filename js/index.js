18aa690fd950bfcb8d64c674563dd38ccd35ee33
KOrsula01840186 <112893823+KOrsula01840186@users.noreply.github.com>
2022-10-23 22:37:40 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,113 +1,114 @@
 //Kevin Orsula, 10/23/2022, GUI-HW3
 //Bunch of variables to use multiple times for getting elements from the HTML form
-var mainForm = document.getElementById("form");
-var mainTable = document.getElementById("mult-table");
-var errorBox = document.getElementById("errors");
-var mainTableContainer = document.getElementById("mult-table-container");
-var multiplierMinimumInput = document.getElementById("min-multiplier");
-var multiplierMaximumInput = document.getElementById("max-multiplier");
-var multiplicandMinimumInput = document.getElementById("min-multiplicand");
-var multiplicandMaximumInput = document.getElementById("max-multiplicand");
+var multiplierMin = document.getElementById("min-multiplier");
+var multiplierMax = document.getElementById("max-multiplier");
+var multiplicandMin = document.getElementById("min-multiplicand");
+var multiplicandMax = document.getElementById("max-multiplicand");
+var inputForm = document.getElementById("form");
+var multTable = document.getElementById("mult-table");
+var errorText = document.getElementById("errors");
 
 main();
 function main() { //This function is where the program begins
-    mainForm.addEventListener("submit", handleSubmitButtonClicked); //Called when the button on the page is clicked, and goes to the handleSubmitButtonClicked function.
+    inputForm.addEventListener("submit", handleSubmitButtonClicked); //Called when the button on the page is clicked, and goes to the handleSubmitButtonClicked function.
 };
 
 function handleSubmitButtonClicked(e) {
     e.preventDefault(); //Used to stop page reload when button is clicked
-    var colMin = +multiplierMinimumInput.value; //Gets values from form
-    var colMax = +multiplierMaximumInput.value;
-    var rowMin = +multiplicandMinimumInput.value;
-    var rowMax = +multiplicandMaximumInput.value;
-    if (!isFormValid(colMin, colMax, rowMin, rowMax)) { //Calls function to check if inputs are valid (ERROR HANDLING)
+    var colMin = +multiplierMin.value; //Gets values from form
+    var colMax = +multiplierMax.value;
+    var rowMin = +multiplicandMin.value;
+    var rowMax = +multiplicandMax.value;
+    if (!errorCheck(colMin, colMax, rowMin, rowMax)) { //Calls function to check if inputs are valid (ERROR HANDLING)
         return;
     }
-    removeErrorIndicators();
+    removeErrors(); //Removes the red outlines that occur on error
     generateTable(colMin, colMax, rowMin, rowMax); //Generates the table
 }
-// ERROR Handeling
-function handleInvalidValueError(element) { //Element is not a number
-    element.style.border = "2px solid red";
-    errorBox.innerHTML += "<p>Error: One of the input values is not an integer.</p><br>";
-}
-function handleInvalidRangeError(element1, element2) { //The two elements from either the rows or the cols provide a bad range
-    element1.style.border = "2px solid red";
-    element2.style.border = "2px solid red";
-    errorBox.innerHTML += "<p>Error: Invalid range provided.</p><br>";
-}
-function handleOutOfRangeError(element) { //Element is greater or lower than the range of -50 to 50
-    element.style.border = "2px solid red";
-    errorBox.innerHTML += "<p>Error: Input values are not in the range (-50, 50).</p><br>";
-}
 
-//ERROR Checking for if value is number, if number is too large or small, and if range is correct
-function isFormValid(min1, max1, min2, max2) {
-    var isValid = true;
-    removeErrorIndicators();
-    if (Number.isNaN(min1)) { 
-        handleInvalidValueError(multiplierMinimumInput);
-        isValid = false;
-    } else if (min1 < -50) {
-        handleOutOfRangeError(multiplierMinimumInput);
-        isValid = false;
-    }
-    if (Number.isNaN(max1)) {
-        handleInvalidValueError(multiplierMaximumInput);
-        isValid = false;
-    } else if (max1 > 50) {
-        handleOutOfRangeError(multiplierMaximumInput);
-        isValid = false;
-    }
-    if (Number.isNaN(min2)) {
-        handleInvalidValueError(multiplicandMinimumInput);
-        isValid = false;
-    } else if (min2 < -50) {
-        handleOutOfRangeError(multiplicandMinimumInput);
-        isValid = false;
-    }
-    if (Number.isNaN(max2)) {
-        handleInvalidValueError(multiplicandMaximumInput);
-        isValid = false;
-    } else if (max2 > 50) {
-        handleOutOfRangeError(multiplicandMaximumInput);
-        isValid = false;
-    }
-    if (min1 >= max1) {
-        handleInvalidRangeError(multiplierMinimumInput, multiplierMaximumInput);
-        isValid = false;
-    }
-    if (min2 >= max2) {
-        handleInvalidRangeError(multiplicandMinimumInput, multiplicandMaximumInput);
-        isValid = false;
-    }
-    return isValid;
-}
-//Used to remove the red border error indicator after an incorect input has been fixed
-function removeErrorIndicators() {
-    //Remove all red borders and error messages from elements
-    multiplicandMaximumInput.style.border = "";
-    multiplierMinimumInput.style.border = "";
-    multiplierMaximumInput.style.border = "";
-    multiplicandMinimumInput.style.border = "";
-    errorBox.innerHTML = "";
-};
 //Generates the table to the page
 function generateTable(colMin, colMax, rowMin, rowMax) {
-    var newHTML = '<tr><td></td>';
-    for( var i = colMin; i <= colMax; i++) {
+    var newHTML = '<tr><td></td>'; //Sets a new table
+    for( var i = colMin; i <= colMax; i++) { //Sets the top row
         var newMult = '<td>'+ i +'</td>';
         newHTML += newMult;
     }
-    for( var y = rowMin; y <= rowMax; y++) {
+    for( var y = rowMin; y <= rowMax; y++) { //Iterates through the table doing the math to fill it
         var newRow = '<tr>';
         newRow += '<td>'+ y +'</td>';
         for( var z = colMin; z <= colMax; z++) {
             newRow += '<td>'+ (y*z) +'</td>';
         }
         newRow += '</tr>';
-        newHTML += newRow;
+        newHTML += newRow; //Adds a new row to the table
     }
-    mainTable.innerHTML = newHTML;
+    multTable.innerHTML = newHTML; //setst the table into the html 
 }
 
+// ERROR Handeling
+function badValueError(element) { //Element is not a number
+    element.style.border = "2px solid red";
+    errorText.innerHTML += "<p>Error: One of the input values is not an integer.</p><br>";
+}
+function badRangeError(element1, element2) { //The two elements from either the rows or the cols provide a bad range
+    element1.style.border = "2px solid red";
+    element2.style.border = "2px solid red";
+    errorText.innerHTML += "<p>Error: Invalid range provided. Min must be less than max.</p><br>";
+}
+function OutOfRangeError(element) { //Element is greater or lower than the range of -50 to 50
+    element.style.border = "2px solid red";
+    errorText.innerHTML += "<p>Error: Input values are not in the range (-50, 50).</p><br>";
+}
+
+//Used to remove the red border error indicator after an incorect input has been fixed
+function removeErrors() {
+    //Remove all red borders and error messages from elements
+    multiplicandMax.style.border = "";
+    multiplierMin.style.border = "";
+    multiplierMax.style.border = "";
+    multiplicandMin.style.border = "";
+    errorText.innerHTML = "";
+};
+
+//ERROR Checking for if value is number, if number is too large or small, and if range is correct
+function errorCheck(min1, max1, min2, max2) {
+    var isValid = true;
+    removeErrors();
+    if (Number.isNaN(min1)) { 
+        badValueError(multiplierMin);
+        isValid = false;
+    } else if (min1 < -50) {
+        OutOfRangeError(multiplierMin);
+        isValid = false;
+    }
+    if (Number.isNaN(max1)) {
+        badValueError(multiplierMax);
+        isValid = false;
+    } else if (max1 > 50) {
+        OutOfRangeError(multiplierMax);
+        isValid = false;
+    }
+    if (Number.isNaN(min2)) {
+        badValueError(multiplicandMin);
+        isValid = false;
+    } else if (min2 < -50) {
+        OutOfRangeError(multiplicandMin);
+        isValid = false;
+    }
+    if (Number.isNaN(max2)) {
+        badValueError(multiplicandMax);
+        isValid = false;
+    } else if (max2 > 50) {
+        OutOfRangeError(multiplicandMax);
+        isValid = false;
+    }
+    if (min1 >= max1) {
+        badRangeError(multiplierMin, multiplierMax);
+        isValid = false;
+    }
+    if (min2 >= max2) {
+        badRangeError(multiplicandMin, multiplicandMax);
+        isValid = false;
+    }
+    return isValid;
+}
